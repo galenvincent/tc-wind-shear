4fe040270d203f17eb35bdea52946534469f2f6f
Galen Vincent <galenbvincent@gmail.com>
2021-01-18 17:56:34 -0700

    Changed password and created local credentials file to hide my password from github.

--- intro.py
+++ intro.py
@@ -1,16 +1,3 @@
-from cartopy.feature import NaturalEarthFeature
-from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
-from datetime import datetime, timedelta
-from matplotlib.colors import LinearSegmentedColormap, from_levels_and_colors
-from metpy.units import units
-from siphon.catalog import TDSCatalog
-from siphon.http_util import session_manager
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
-import matplotlib.pyplot as plt
-import matplotlib.ticker as mticker
-import metpy.calc as mpcalc
-import numpy as np
-import os
-import sys
+import credentials
+
 
